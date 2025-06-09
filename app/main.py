b6b62af1b24e303bb06daff627a1c0cc366bf123
Silvia Cue <silviacue@icloud.com>
2025-06-09 17:41:19 +0200

    Creación de carpetas  e instalaciones

---
+++ app/main.py
@@ -0,0 +1,12 @@
+# app/main.py
+from fastapi import FastAPI
+from app.routers import upload  # puedes ir agregando los demás luego
+
+app = FastAPI(title="RAG API")
+
+# Registrar rutas
+app.include_router(upload.router)
+
+@app.get("/")
+def root():
+    return {"message": "Hola mundo desde FastAPI + RAG"}
