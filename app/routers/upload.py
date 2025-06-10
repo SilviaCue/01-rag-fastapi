14a0b63be190641bf7916b99767e59b819367bc9
Silvia Cue <silviacue@icloud.com>
2025-06-10 10:35:58 +0200

    Creación y prueba de file_parser.py

--- app/routers/upload.py
+++ app/routers/upload.py
@@ -7,14 +7,17 @@
 from fastapi import APIRouter, UploadFile, File
 import shutil
 import os
+from app.services.file_parser import extract_text_from_pdf  #  importar parser
 
 router = APIRouter()
 
 # Ruta absoluta basada en la ubicación real del archivo
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", "..", "storage", "docs_raw"))
+CHUNK_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", "..", "storage", "docs_chunks"))
 
 os.makedirs(UPLOAD_DIR, exist_ok=True)
+os.makedirs(CHUNK_DIR, exist_ok=True)
 
 @router.post("/upload/")
 async def upload_document(file: UploadFile = File(...)):
@@ -23,8 +26,27 @@ async def upload_document(file: UploadFile = File(...)):
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    print("DEBUG — guardado en:", file_path)
-    return {"filename": file.filename}
+    #Extraer texto
+    text = extract_text_from_pdf(file_path)
+    
+    # Guardar texto en archivo .txt
+    txt_filename = os.path.splitext(file.filename)[0] + ".txt"
+    txt_path = os.path.join(CHUNK_DIR, txt_filename)
+    with open(txt_path, "w", encoding="utf-8") as txt_file:
+        txt_file.write(text)
+        
+    print("Guardado PDF:", file_path)
+    print("Texto extraído en:", txt_path)
+
+    return {
+        "filename": file.filename,
+        "text_file": txt_filename
+    }
+
+
+
+
+
 
 
 
--- app/services/file_parser.py
+++ app/services/file_parser.py
@@ -6,8 +6,8 @@ import fitz  # PyMuPDF
 import os
 
 def extract_text_from_pdf(file_path: str) -> str:
-    doc = fitz.open(file_path)
     text = ""
-    for page in doc:
-        text += page.get_text()
+    with fitz.open(file_path) as doc:
+        for page in doc:
+            text += page.get_text()
     return text
